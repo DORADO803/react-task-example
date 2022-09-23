eea8b8448e3739c9f8958990ee2abb69d403fc73
Juan Manuel <80168390+DORADO803@users.noreply.github.com>
2022-09-22 22:38:38 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,7 @@
+export const tasks=[
+  { id: 0, title: "Mi primera tarea", description: "Mi primera tarea" },
+
+  { id: 1, title: "Mi primera tarea", description: "Mi primera tarea" },
+
+  { id: 2, title: "Mi primera tarea", description: "Mi primera tarea" },
+];
